5d2950bf9e0d05b2136ca9caee053890cb7cf212
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-11 17:19:22 +0900

    Implement config existence validator

--- lib/dotremap.rb
+++ lib/dotremap.rb
@@ -1,5 +1,33 @@
 require "dotremap/version"
+require "unindent"
 
-module Dotremap
-  # Your code goes here...
+class Dotremap
+  def initialize(config_path)
+    @config_path = config_path
+  end
+  attr_reader :config_path
+
+  def compile
+    validate_config_existence
+  end
+
+  def replace_private_xml
+  end
+
+  private
+
+  def validate_config_existence
+    return if File.exists?(config_path)
+
+    File.write(config_path, <<-EOS.unindent)
+      #!/usr/bin/env ruby
+
+      # # Example
+      # item do
+      #   name 'Command+K to Command+L'
+      #   identifier 'remap.command_k_to_command_l'
+      #   autogen '__KeyToKey__ KeyCode::K, VK_COMMAND, KeyCode::L, VK_COMMAND'
+      # end
+    EOS
+  end
 end
--- lib/dotremap/version.rb
+++ lib/dotremap/version.rb
@@ -1,3 +1,3 @@
-module Dotremap
+class Dotremap
   VERSION = "0.0.1"
 end
