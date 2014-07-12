c7bd5fd4fe91c7ac6450eb07861fae5297a699c4
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-12 16:02:56 +0900

    Sophisticate key converter

--- lib/dotremap/remap.rb
+++ lib/dotremap/remap.rb
@@ -1,4 +1,12 @@
 class Dotremap::Remap
+  KEYCODE_MAP = {
+    "Up"    => "CURSOR_UP",
+    "Down"  => "CURSOR_DOWN",
+    "Right" => "CURSOR_RIGHT",
+    "Left"  => "CURSOR_LEFT",
+  }.freeze
+  KEY_EXPRESSION = "([A-Z]|#{KEYCODE_MAP.keys.join('|')})"
+
   def initialize(from, to)
     @from = from
     @to = to
@@ -11,10 +19,36 @@ class Dotremap::Remap
   private
 
   def from
-    @from
+    key_combination(@from)
   end
 
   def to
-    @to
+    key_combination(@to)
+  end
+
+  def key_combination(raw_combination)
+    raw_key = raw_combination.match(/#{KEY_EXPRESSION}$/).to_s
+
+    case raw_combination
+    when /^Cmd-#{KEY_EXPRESSION}$/
+      "#{key_expression(raw_key)}, VK_COMMAND"
+    when /^Cmd-Opt-#{KEY_EXPRESSION}$/
+      "#{key_expression(raw_key)}, VK_COMMAND | VK_OPTION"
+    when /^#{KEY_EXPRESSION}$/
+      key_expression(raw_key)
+    else
+      raw_combination
+    end
+  end
+
+  def key_expression(raw_key)
+    case raw_key
+    when /^[A-Z]$/
+      "KeyCode::#{raw_key}"
+    when /^(#{KEYCODE_MAP.keys.join('|')})$/
+      "KeyCode::#{KEYCODE_MAP[raw_key]}"
+    else
+      raw_key
+    end
   end
 end
