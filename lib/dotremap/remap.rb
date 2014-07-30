de9f7779d89cfeb3903f84b2dfe6ac1a3d45d0ba
Takashi Kokubun <takashikkbn@gmail.com>
2014-07-31 01:40:13 +0900

    Refactor Dotremap::Remap

--- lib/dotremap/remap.rb
+++ lib/dotremap/remap.rb
@@ -1,22 +1,16 @@
 require "dotremap/key"
+require "dotremap/property"
 
-class Dotremap::Remap
+class Dotremap::Remap < Dotremap::Property
   def initialize(from, to)
-    @from = from
-    @to = to
-  end
+    tos = [to].flatten
 
-  def to_xml
-    "<autogen>__KeyToKey__ #{from}, #{to}</autogen>"
-  end
-
-  private
-
-  def from
-    Dotremap::Key.new(@from)
-  end
-
-  def to
-    [@to].flatten.map { |to| Dotremap::Key.new(to) }.join(", ")
+    super(
+      "autogen",
+      [
+        "__KeyToKey__ #{Dotremap::Key.new(from)}",
+        *tos.map { |to| Dotremap::Key.new(to) },
+      ].join(", "),
+    )
   end
 end
